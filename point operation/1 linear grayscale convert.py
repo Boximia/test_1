fe0c0fc3d38e1a71bf830d68f6357febc93720ae
Boximia <86640571+Boximia@users.noreply.github.com>
2023-06-20 20:53:36 +0800

    Add files via upload

---
+++ point operation/1 linear grayscale convert.py
@@ -0,0 +1,29 @@
+# -- coding: utf-8 --
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+img=cv2.imread("gwl.jpg",0)
+
+# 获取图像高度和宽度
+height = img.shape[0]
+width = img.shape[1]
+
+#创建一幅图像， uint8是专门用于存储各种图像的（包括RGB，灰度图像等），范围是从0–255
+result = np.zeros((height, width), np.uint8)
+
+# 图像灰度反转
+for i in range(height):
+    for j in range(width):
+        gray = -(img[i, j])+255
+        result[i, j] = np.uint8(gray)
+
+# 显示图形
+plt.figure(num='comparison')
+titles = ['gray Image', 'gray scale inversion']
+images = [img, result]
+for i in range(2):
+    plt.subplot(1, 2, i + 1)
+    plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+plt.show()
